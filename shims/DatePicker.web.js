1ec232640911cbbaaaaf2ad24b4de679b6d52d03
Mark Vayngrib <mark@tradle.io>
2017-04-02 23:31:54 -0400

    publish: Merge remote-tracking branch 'origin/master' into web
    generated from commit da2349da622a7bff6a4f4ef3bb4af886321380d2

---
+++ shims/DatePicker.web.js
@@ -0,0 +1,50 @@
+'use strict'
+
+import {
+  View,
+  StyleSheet,
+  TouchableHighlight,
+  Text
+} from 'react-native'
+
+import React, { Component } from 'react'
+// import DatePicker from 'react-datepicker/dist/react-datepicker'
+// require('react-datepicker/dist/react-datepicker.css')
+
+import moment from 'moment'
+import dateformat from 'dateformat'
+import Actions from '../../Actions/Actions'
+import createCalendarModal from '../../Components/CalendarModal'
+
+export default class DatePickerAdapter extends Component {
+  onPress() {
+    const { onDateChange } = this.props
+    Actions.showModal({
+      contents: createCalendarModal({
+        selectedDate: this.props.date,
+        onConfirm: function (date) {
+          Actions.hideModal()
+          onDateChange(new Date(date))
+        },
+        onCancel: function () {
+          Actions.hideModal()
+        }
+      })
+    })
+  }
+  render() {
+    const { date, customStyles } = this.props
+    const dateString = date ? dateformat(date, 'mmm dS, yyyy') : this.props.placeholder
+    const textStyle = customStyles && customStyles.placeholderText
+    return (
+      <TouchableHighlight underlayColor="transparent" style={this.props.style} onPress={() => this.onPress()}>
+        <Text style={textStyle}>{dateString}</Text>
+      </TouchableHighlight>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  fix: {
+  }
+})
---
+++ shims/no-push.js
@@ -0,0 +1,78 @@
+import { EventEmitter } from 'events'
+import { AppState } from 'react-native'
+import { constants } from '@tradle/engine'
+import Actions from '../../Actions/Actions'
+
+const { TYPE } = constants
+const IGNORE_TYPES = [
+  'tradle.CustomerWaiting'
+]
+
+const notificationsSupported = 'Notification' in global
+if (notificationsSupported) requestPermissions()
+
+let tabIsFocused = AppState.currentState === 'active'
+let me
+
+AppState.addEventListener('change', function () {
+  tabIsFocused = AppState.currentState === 'active'
+  if (tabIsFocused && me && me.unreadPushNotifications) {
+    Actions.updateMe({ unreadPushNotifications: 0 })
+  }
+})
+
+const emitter = module.exports = exports = new EventEmitter()
+
+emitter.init = function (opts) {
+  if (!notificationsSupported) return Promise.resolve()
+
+  me = opts.me
+  const node = opts.node
+  node.on('message', msg => {
+    if (tabIsFocused || Notification.permission !== 'granted') return
+
+    // embedded messages are an extra layer deep
+    const obj = msg.object.object || msg.object
+    const type = obj[TYPE]
+    if (IGNORE_TYPES.indexOf(type) !== -1) return
+
+    // TODO: show logo of msg sender
+    const n = new Notification('You have a message waiting!', {
+      body: 'Click to open your Tradle tab',
+      icon: '/img/logo.png'
+    })
+
+    n.onclick = function () {
+      try {
+        window.focus()
+        Actions.viewChat(msg)
+      } catch (e) {}
+
+      clearTimeout(closeTimeout)
+      n.close()
+    }
+
+    const closeTimeout = setTimeout(() => n.close(), 5000)
+  })
+}
+
+emitter.subscribe = function (publisher) {
+  return Promise.resolve()
+}
+
+emitter.resetBadgeNumber = function () {
+  return Promise.resolve()
+}
+
+function requestPermissions () {
+  if (Notification.permission === 'granted') return
+
+  // Let's check whether notification permissions have already been granted
+  if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      if (permission === 'granted') {
+        console.log('notifications permission granted')
+      }
+    })
+  }
+}
